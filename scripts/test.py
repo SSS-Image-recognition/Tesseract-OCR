57e075f83ab320f679e5b97fcfc5e463a51f5a6c
NaokiHatta <naokisub00@gmail.com>
2021-01-17 19:24:18 -0800

    utilityの完成版

---
+++ scripts/test.py
@@ -0,0 +1,31 @@
+import imageio
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from PIL import Image
+import cv2
+import pyocr
+import os
+from utility import utility
+
+
+img = cv2.imread('IMG.png')
+img_org = Image.open('IMG.png')
+tools = pyocr.get_available_tools()
+tool = tools[0]
+
+u = utility()
+
+message,stickerId,stickerPackageId,index = u.check_text_stamp_detection(img)
+
+sticker_list_index = 0
+for i in index:
+    message.insert(i,stickerId[sticker_list_index])
+    message.insert(i,stickerPackageId[sticker_list_index])
+    sticker_list_index += 1
+'''
+messageは配列で、画像を上から1行ずつ読み取った結果を格納しています。
+文章のときはその認識結果を、
+スタンプのときはstickerpackageID,stickerIDの順に配列に格納しています
+'''
+print(message)
